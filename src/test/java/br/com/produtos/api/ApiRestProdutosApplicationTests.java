844050a9489e2a87bb369844ce41d621165c92a6
Felipe Chagas de Souza <chagas.felipe.1989@gmail.com>
2021-09-05 19:02:28 -0300

    Setup Projeto Maven

---
+++ src/test/java/br/com/produtos/api/ApiRestProdutosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.produtos.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestProdutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
