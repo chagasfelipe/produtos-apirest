cd91135590e58402c9b0042617e6265941e68a01
Felipe Chagas de Souza <chagas.felipe.1989@gmail.com>
2021-09-05 20:43:06 -0300

    Alteração URI controller

--- src/main/java/br/com/produtos/apirest/controller/ProdutoController.java
+++ src/main/java/br/com/produtos/apirest/controller/ProdutoController.java
@@ -11,7 +11,7 @@ import javax.validation.Valid;
 import java.util.List;
 
 @RestController
-@RequestMapping("/produtos")
+@RequestMapping("/api/produtos")
 public class ProdutoController {
 
     @Autowired
