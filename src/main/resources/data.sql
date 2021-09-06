d13b40e705baf003aa7c4fbc28f5bf25154f99aa
Felipe Chagas de Souza <chagas.felipe.1989@gmail.com>
2021-09-05 21:42:49 -0300

    Alteração Configuração Hibernate Application.properties

--- src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO TB_PRODUTO VALUES (1,'Descrição Produto 1', 'Nome Produto 1',2.99 );
-INSERT INTO TB_PRODUTO VALUES (2,'Descrição Produto 2', 'Nome Produto 2',12.99 );
-INSERT INTO TB_PRODUTO VALUES (3,'Descrição Produto 3', 'Nome Produto 3',7.99 );
