2fc572044839f2a5bd88f78f6f3febba135607e7
Felipe Chagas de Souza <chagas.felipe.1989@gmail.com>
2021-09-05 21:04:51 -0300

    Alteração script carga banco de dados

---
+++ src/main/resources/sql/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO TB_PRODUTO (nome, valorUnitario, descricao) VALUES ("Nome Produto 1",2.99 ,"Descrição Produto 1 ");
+INSERT INTO TB_PRODUTO (nome, valorUnitario, descricao) VALUES ("Nome Produto 2",2.99 ,"Descrição Produto 2 ");
+INSERT INTO TB_PRODUTO (nome, valorUnitario, descricao) VALUES ("Nome Produto 3",2.99 ,"Descrição Produto 3 ");
+INSERT INTO TB_PRODUTO (nome, valorUnitario, descricao) VALUES ("Nome Produto 4",2.99 ,"Descrição Produto 4 ");
+INSERT INTO TB_PRODUTO (nome, valorUnitario, descricao) VALUES ("Nome Produto 5",2.99 ,"Descrição Produto 5 ");
+INSERT INTO TB_PRODUTO (nome, valorUnitario, descricao) VALUES ("Nome Produto 6",2.99 ,"Descrição Produto 6 ");
