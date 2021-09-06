dea958a354de03fe3f29e2b11f2911cb06138340
Felipe Chagas de Souza <chagas.felipe.1989@gmail.com>
2021-09-05 22:37:07 -0300

    Adicionando Collection do Postman para testar a API.

--- src/main/java/br/com/produtos/apirest/service/impl/ProdutoServiceImpl.java
+++ src/main/java/br/com/produtos/apirest/service/impl/ProdutoServiceImpl.java
@@ -6,11 +6,6 @@ import br.com.produtos.apirest.service.ProdutoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
-import java.net.URI;
 import java.util.List;
 
 @Service
